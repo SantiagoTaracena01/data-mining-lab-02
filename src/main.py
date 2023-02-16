e91a4bfa635dac80ccd90e4d72b9e72a30d6a5e4
unknown <staracenapuga@gmail.com>
2023-02-16 14:08:03 -0600

    Initialized repo

---
+++ src/main.py
@@ -0,0 +1,11 @@
+"""
+Universidad del Valle de Guatemala
+Minería de Datos
+Laboratorio 02
+Marco Orozco (20857) y Santiago Taracena (20017)
+"""
+
+import pandas as pd
+
+data = pd.read_csv("./data/breast-cancer-wisconsin.csv")
+print(data.head())

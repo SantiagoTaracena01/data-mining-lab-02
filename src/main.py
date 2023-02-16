3e0c8b924e726629f9371ec367ac6f16d6bd7265
unknown <staracenapuga@gmail.com>
2023-02-16 16:38:12 -0600

    bare_nuclei cleanse

--- src/main.py
+++ src/main.py
@@ -8,4 +8,10 @@ Marco Orozco (20857) y Santiago Taracena (20017)
 import pandas as pd
 
 data = pd.read_csv("./data/breast-cancer-wisconsin.csv")
+
+data["bare_nuclei"] = data["bare_nuclei"].apply(lambda x: 0 if (x == "?") else int(x))
+
 print(data.head())
+print(data.isnull().mean())
+print(data.dtypes)
+print(data.describe())
